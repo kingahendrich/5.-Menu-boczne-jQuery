3f02093a91f0596b6011ec6961245a3b5ab6da69
Kinga Hndr <ingainmainz@gmail.com>
2024-06-16 19:12:43 +0200

    menu boczne gotowe

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.hamburger').click(function(){
+        $('.main-menu').toggleClass('open'); //jesli klasa jest to usunie jesli nie ma to doda
+    });
+
+    $('.main-menu-item a').click(function(e) {
+        e.preventDefault
+
+        $('body').fadeOut(2000);
+    });
+
+})
